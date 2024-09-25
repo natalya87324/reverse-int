93b536369d7d6d3e3c826d3c2e61ed3393cb3ae0
Natalya <kim87324@yandex.ru>
2024-09-25 21:47:39 +0300

    add solution to reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const str = n.toString();
+    let reverse = "";
+
+    for (let i = str.length - 1; i >= 0; i -= 1) {
+        reverse += str[i];
+    }
+
+    return parseInt(reverse, 10);
+};
